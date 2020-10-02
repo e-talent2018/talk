9396c31763ce56c866097361b65c3c0b3d9c825e
Wyatt Johnson <wyatt.johnson@voxmedia.com>
2020-10-02 12:44:34 -0600

    feat: initial ratings and reviews feature

--- src/core/server/graph/resolvers/index.ts
+++ src/core/server/graph/resolvers/index.ts
@@ -53,6 +53,7 @@ import { SlackConfiguration } from "./SlackConfiguration";
 import { SSOAuthIntegration } from "./SSOAuthIntegration";
 import { StaffConfiguration } from "./StaffConfig";
 import { Story } from "./Story";
+import { StoryRatings } from "./StoryRatings";
 import { StorySettings } from "./StorySettings";
 import { Subscription } from "./Subscription";
 import { SuspensionStatus } from "./SuspensionStatus";
@@ -122,6 +123,7 @@ const Resolvers: GQLResolver = {
   SlackConfiguration,
   SSOAuthIntegration,
   Story,
+  StoryRatings,
   StorySettings,
   Subscription,
   SuspensionStatus,
